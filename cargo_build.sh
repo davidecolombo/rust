d1e079d31e32adeb7de7af1826fcb9f68f193af1
Davide Colombo <2599183+davidecolombo@users.noreply.github.com>
2022-04-11 22:57:15 +0200

    minor changes to cargo build

--- cargo_build.sh
+++ cargo_build.sh
@@ -5,3 +5,7 @@ cargo build
 #cargo build --release
 #cargo check
 #cargo run
+
+# Intelli IDEA + Rust plugin
+#cargo build --color=always --message-format=json-diagnostic-rendered-ansi --package rust_samples --bin rust_samples
+#cargo run --color=always --package rust_samples --bin rust_samples
